a36355cc6db1adacf434c8789059f7302ce2b616
Nashalan <sivalingamnashalan@gmail.com>
2025-11-02 18:44:26 +0800

    Remove duplicate import of matplotlib.pyplot

--- Page 1.py
+++ Page 1.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-import matplotlib.pyplot as plt
 import plotly.express as px
 
 st.title("🎯 Objective 1: Explore Academic Stress Distribution")
